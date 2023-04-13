bf676ade32a4edf1991dd9cbdf90e3ef7b5aa77a
Lucas Conde <lucascondeferreira@gmail.com>
2023-04-13 11:39:55 -0300

    --v0.0.7 Atualizando página de trabalhos e atualizando função de infinite scrool

--- js/script.js
+++ js/script.js
@@ -9,78 +9,81 @@ const images = [
     // adicione mais objetos de imagem aqui, se necessário
   ];
   
-  // Configuração do infinite scroll
-  const imagesPerBatch = 4; // Quantidade de imagens por lote
-  let currentBatch = 0; // Lote atual
-  const imageContainer = document.getElementById('image-container');
-  const loadingIndicator = document.getElementById('loading-indicator');
-  // Função para criar o elemento de imagem com título e link
+// Configuração do infinite scroll
+const imagesPerBatch = 4; // Quantidade de imagens por lote
+let currentBatch = 0; // Lote atual
+const imageContainer = document.getElementById('image-container');
+const loadingIndicator = document.getElementById('loading-indicator');
+const imageContainerHeight = imageContainer.offsetHeight; // Altura do container de imagens
+let loading = false; // Variável para verificar se está carregando
+
+// Função para criar o elemento de imagem com título e link
 function createImageElement(imageData) {
-    // Criar elemento de imagem
-    const img = document.createElement('img');
-    img.src = imageData.src;
-    img.alt = imageData.title;
-  
-    // Adicionar classe à imagem
-    img.classList.add('principal'); // Classe personalizada para a imagem
-  
-    // Criar elemento de título
-    const title = document.createElement('h2');
-    title.textContent = imageData.title;
-  
-    // Adicionar classe ao título
-    title.classList.add('principal'); // Classe personalizada para o título
-  
-    // Criar elemento de link
-    const link = document.createElement('a');
-    link.href = '#'; // Alterar o link para a página desejada
-    link.classList.add('btn'); // Adicionar classe ao link
-    link.addEventListener('click', () => {
-      // Função para buscar informações do JSON com base no ID da imagem clicada
-      fetch('./imagens.json')
-        .then(response => response.json())
-        .then(data => {
-          const imageInfo = data.find(item => item.id === imageData.id);
-          if (imageInfo) {
-            // Aqui você pode acessar as informações do JSON e fazer o que desejar
-            console.log('Título:', imageInfo.title);
-            console.log('Texto 1:', imageInfo.texto1);
-            console.log('Texto 2:', imageInfo.texto2);
-            console.log('Imagens:', imageInfo.imagens);
-          }
-        })
-        .catch(error => console.error(error));
-    });
-    link.appendChild(img); // Adicionar imagem como filho do link
-    link.appendChild(title); // Adicionar título como filho do link
-  
-    // Criar container para imagem, título e link
-    const container = document.createElement('div');
-    container.classList.add('cards-trabalhos', 'col-lg-5'); // Adicionar classes ao container
-    container.appendChild(link); // Adicionar link como filho do container
-  
-    // Adicionar container à galeria
-    imageContainer.appendChild(container);
-  
-    // Adicionar evento de clique para as imagens
-    img.addEventListener('click', function() {
-      const imageId = imageData.id; // Corrigir a obtenção do ID da imagem
-      const imageInfo = getImageInfo(imageId); // Obter informações da imagem com base no ID
-      if (imageInfo) {
-        showImageDetails(imageInfo); // Exibir detalhes da imagem em um modal ou pop-up
-      }
-    });
-  }
-  
-  // Função para obter informações da imagem com base no ID
-  function getImageInfo(imageId) {
-    for (let i = 0; i < images.length; i++) {
-      if (images[i].id === imageId) {
-        return images[i]; // Retornar informações da imagem
-      }
+  // Criar elemento de imagem
+  const img = document.createElement('img');
+  img.src = imageData.src;
+  img.alt = imageData.title;
+
+  // Adicionar classe à imagem
+  img.classList.add('principal'); // Classe personalizada para a imagem
+
+  // Criar elemento de título
+  const title = document.createElement('h2');
+  title.textContent = imageData.title;
+
+  // Adicionar classe ao título
+  title.classList.add('principal'); // Classe personalizada para o título
+
+  // Criar elemento de link
+  const link = document.createElement('a');
+  link.href = '#'; // Alterar o link para a página desejada
+  link.classList.add('btn'); // Adicionar classe ao link
+  link.addEventListener('click', () => {
+    // Função para buscar informações do JSON com base no ID da imagem clicada
+    fetch('./imagens.json')
+      .then(response => response.json())
+      .then(data => {
+        const imageInfo = data.find(item => item.id === imageData.id);
+        if (imageInfo) {
+          // Aqui você pode acessar as informações do JSON e fazer o que desejar
+          console.log('Título:', imageInfo.title);
+          console.log('Texto 1:', imageInfo.texto1);
+          console.log('Texto 2:', imageInfo.texto2);
+          console.log('Imagens:', imageInfo.imagens);
+        }
+      })
+      .catch(error => console.error(error));
+  });
+  link.appendChild(img); // Adicionar imagem como filho do link
+  link.appendChild(title); // Adicionar título como filho do link
+
+  // Criar container para imagem, título e link
+  const container = document.createElement('div');
+  container.classList.add('cards-trabalhos', 'col-lg-5'); // Adicionar classes ao container
+  container.appendChild(link); // Adicionar link como filho do container
+
+  // Adicionar container à galeria
+  imageContainer.appendChild(container);
+
+  // Adicionar evento de clique para as imagens
+  img.addEventListener('click', function() {
+    const imageId = imageData.id; // Corrigir a obtenção do ID da imagem
+    const imageInfo = getImageInfo(imageId); // Obter informações da imagem com base no ID
+    if (imageInfo) {
+      showImageDetails(imageInfo); // Exibir detalhes da imagem em um modal ou pop-up
     }
-    return null; // Retornar null caso não seja encontrada nenhuma imagem com o ID fornecido
+  });
+}
+
+// Função para obter informações da imagem com base no ID
+function getImageInfo(imageId) {
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].id === imageId) {
+      return images[i]; // Retornar informações da imagem
+    }
   }
+  return null; // Retornar null caso não seja encontrada nenhuma imagem com o ID fornecido
+}
   
   // Função para exibir os detalhes da imagem em uma nova página
   function showImageDetails(imageData) {
@@ -125,7 +128,7 @@ function createImageElement(imageData) {
       currentBatch += imagesPerBatch;
   
       hideLoadingIndicator();
-    }, 1000);
+    }, 2000);
   }
   
   // Função para verificar se o scroll chegou ao fim da página
@@ -146,14 +149,31 @@ function createImageElement(imageData) {
   function showLoadingIndicator() {
     loadingIndicator.style.display = 'block';
   }
-  
-  // Função para ocultar o indicador de carregamento
-  function hideLoadingIndicator() {
+ // Função para ocultar o indicador de carregamento
+ function hideLoadingIndicator() {
     loadingIndicator.style.display = 'none';
-}
+  }
+
+  // Função para verificar se o scroll chegou ao fim do container de imagens
+  function checkScrollEnd() {
+    const containerHeight = imageContainer.offsetHeight;
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    if (scrollTop + windowHeight >= containerHeight) {
+      // Próximo lote de imagens
+      loadImagesBatch();
+    }
+  }
+
+  // Adicionar evento de scroll ao container de imagens
+  imageContainer.addEventListener('scroll', () => {
+    checkScrollEnd();
+  });
+
 
 // Event listener para verificar o scroll da página
 window.addEventListener('scroll', checkScrollEnd);
 
 // Inicialização do carregamento das imagens
 loadImagesBatch();
+
