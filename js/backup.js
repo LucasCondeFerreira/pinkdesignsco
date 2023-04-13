2a32c019428c961fd60693cc4c5818e98c80bd1e
Lucas Conde <lucascondeferreira@gmail.com>
2023-04-13 11:31:34 -0300

    --v0.0.5 Atualizando página de trabalhos e adicionando função de detalhes das imagens

--- js/backup.js
+++ js/backup.js
@@ -1,21 +1,23 @@
 // Dados das imagens
 const images = [
-    { id: 1, src: '../assets/images/point-pizzaria.JPG', title: 'Point Pizzaria', link: '#' },
-    { id: 2, src: '../assets/images/pod-falar.JPG', title: 'PodFalar', link: '#' },
-    { id: 3, src: '../assets/images/IMG-6444.JPG', title: 'Booster Social', link: '#' },
-    { id: 4, src: '../assets/images/celeiro.JPG', title: 'Celeiro', link: '#' },
-    { id: 5, src: '../assets/images/midia-social-pacote.JPEG', title: 'Lady Hair', link: '#' },
-    { id: 6, src: '../assets/images/IMG-2000.JPEG', title: 'Angels Fusion', link: '#' }
+    { src: '../assets/images/point-pizzaria.JPG', title: 'Point Pizzaria', link: '#' },
+    { src: '../assets/images/pod-falar.JPG', title: 'PodFalar', link: '#' },
+    { src: '../assets/images/IMG-6444.JPG', title: 'Booster Social', link: '#' },
+    { src: '../assets/images/celeiro.JPG', title: 'Celeiro', link: '#' },
+    { src: '../assets/images/midia-social-pacote.JPEG', title: 'Lady Hair', link: '#' },
+    { src: '../assets/images/IMG-2000.JPEG', title: 'Angels Fusion', link: '#' },
     // adicione mais objetos de imagem aqui, se necessário
   ];
-  
+
   // Configuração do infinite scroll
   const imagesPerBatch = 4; // Quantidade de imagens por lote
   let currentBatch = 0; // Lote atual
   const imageContainer = document.getElementById('image-container');
   const loadingIndicator = document.getElementById('loading-indicator');
+  
+  
   // Função para criar o elemento de imagem com título e link
-function createImageElement(imageData) {
+  function createImageElement(imageData) {
     // Criar elemento de imagem
     const img = document.createElement('img');
     img.src = imageData.src;
@@ -33,24 +35,8 @@ function createImageElement(imageData) {
   
     // Criar elemento de link
     const link = document.createElement('a');
-    link.href = '#'; // Alterar o link para a página desejada
+    link.href = imageData.link; // Usar o link do array de imagens
     link.classList.add('btn'); // Adicionar classe ao link
-    link.addEventListener('click', () => {
-      // Função para buscar informações do JSON com base no ID da imagem clicada
-      fetch('./imagens.json')
-        .then(response => response.json())
-        .then(data => {
-          const imageInfo = data.find(item => item.id === imageData.id);
-          if (imageInfo) {
-            // Aqui você pode acessar as informações do JSON e fazer o que desejar
-            console.log('Título:', imageInfo.title);
-            console.log('Texto 1:', imageInfo.texto1);
-            console.log('Texto 2:', imageInfo.texto2);
-            console.log('Imagens:', imageInfo.imagens);
-          }
-        })
-        .catch(error => console.error(error));
-    });
     link.appendChild(img); // Adicionar imagem como filho do link
     link.appendChild(title); // Adicionar título como filho do link
   
@@ -61,56 +47,8 @@ function createImageElement(imageData) {
   
     // Adicionar container à galeria
     imageContainer.appendChild(container);
-  
-    // Adicionar evento de clique para as imagens
-    img.addEventListener('click', function() {
-      const imageId = imageData.id; // Corrigir a obtenção do ID da imagem
-      const imageInfo = getImageInfo(imageId); // Obter informações da imagem com base no ID
-      if (imageInfo) {
-        showImageDetails(imageInfo); // Exibir detalhes da imagem em um modal ou pop-up
-      }
-    });
   }
   
-  // Função para obter informações da imagem com base no ID
-  function getImageInfo(imageId) {
-    for (let i = 0; i < images.length; i++) {
-      if (images[i].id === imageId) {
-        return images[i]; // Retornar informações da imagem
-      }
-    }
-    return null; // Retornar null caso não seja encontrada nenhuma imagem com o ID fornecido
-  }
-  
-  // Função para exibir os detalhes da imagem em uma nova página
-  function showImageDetails(imageData) {
-    const title = imageData.title;
-    const texto1 = imageData.texto1;
-    const texto2 = imageData.texto2;
-    const imagens = imageData.imagens;
-  
-    // Criar uma nova janela do navegador
-    const newWindow = window.open('', '_blank');
-    newWindow.document.write(`
-      <html>
-      <head>
-        <title>${title}</title>
-      </head>
-      <body>
-        <h1>${title}</h1>
-        <p>${texto1}</p>
-        <p>${texto2}</p>
-      <h2>Imagens:</h2>
-      <ul>
-        ${imagens.map(imagem => `<li><img src="${imagem.src}" alt="${imagem.alt}"></li>`).join('')}
-      </ul>
-    </body>
-    </html>
-  `);
-  newWindow.document.close();
-}
-
-
   // Função para carregar lote de imagens
   function loadImagesBatch() {
     showLoadingIndicator();
