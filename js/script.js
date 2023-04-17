c4ce9a296c2e4093f0c86c240b45c36f8193ff7f
Lucas Conde <lucascondeferreira@gmail.com>
2023-04-16 21:47:42 -0300

    --v 0.0.8 Atualizando página de trabalhos e Criando Página de destino

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Dados das imagens
 const images = [
-    { id: 1, src: '../assets/images/point-pizzaria.JPG', title: 'Point Pizzaria', link: '#' },
+    { id: 1, src: '../assets/images/academia-12.png', title: 'Movimento Bem Estar', link: '#' },
     { id: 2, src: '../assets/images/pod-falar.JPG', title: 'PodFalar', link: '#' },
     { id: 3, src: '../assets/images/IMG-6444.JPG', title: 'Booster Social', link: '#' },
     { id: 4, src: '../assets/images/celeiro.JPG', title: 'Celeiro', link: '#' },
@@ -36,7 +36,7 @@ function createImageElement(imageData) {
 
   // Criar elemento de link
   const link = document.createElement('a');
-  link.href = '#'; // Alterar o link para a página desejada
+  link.href = 'destino.html?id=' + imageData.id; // Link que envia o ID da imagem
   link.classList.add('btn'); // Adicionar classe ao link
   link.addEventListener('click', () => {
     // Função para buscar informações do JSON com base no ID da imagem clicada
@@ -45,15 +45,13 @@ function createImageElement(imageData) {
       .then(data => {
         const imageInfo = data.find(item => item.id === imageData.id);
         if (imageInfo) {
-          // Aqui você pode acessar as informações do JSON e fazer o que desejar
-          console.log('Título:', imageInfo.title);
-          console.log('Texto 1:', imageInfo.texto1);
-          console.log('Texto 2:', imageInfo.texto2);
-          console.log('Imagens:', imageInfo.imagens);
+          // Redirecionar para a página de destino com as informações da imagem
+          window.location.href = `destino.html?id=${imageInfo.id}&title=${imageInfo.title}&texto1=${imageInfo.texto1}&texto2=${imageInfo.texto2}&imagens=${encodeURIComponent(JSON.stringify(imageInfo.imagens))}`;
         }
       })
       .catch(error => console.error(error));
   });
+  
   link.appendChild(img); // Adicionar imagem como filho do link
   link.appendChild(title); // Adicionar título como filho do link
 
@@ -64,55 +62,7 @@ function createImageElement(imageData) {
 
   // Adicionar container à galeria
   imageContainer.appendChild(container);
-
-  // Adicionar evento de clique para as imagens
-  img.addEventListener('click', function() {
-    const imageId = imageData.id; // Corrigir a obtenção do ID da imagem
-    const imageInfo = getImageInfo(imageId); // Obter informações da imagem com base no ID
-    if (imageInfo) {
-      showImageDetails(imageInfo); // Exibir detalhes da imagem em um modal ou pop-up
-    }
-  });
 }
-
-// Função para obter informações da imagem com base no ID
-function getImageInfo(imageId) {
-  for (let i = 0; i < images.length; i++) {
-    if (images[i].id === imageId) {
-      return images[i]; // Retornar informações da imagem
-    }
-  }
-  return null; // Retornar null caso não seja encontrada nenhuma imagem com o ID fornecido
-}
-  
-  // Função para exibir os detalhes da imagem em uma nova página
-  function showImageDetails(imageData) {
-    const title = imageData.title;
-    const texto1 = imageData.texto1;
-    const texto2 = imageData.texto2;
-    const imagens = imageData.imagens;
-  
-    // Criar uma nova janela do navegador
-    const newWindow = window.open('', '_blank');
-    newWindow.document.write(`
-      <html>
-      <head>
-        <title>${title}</title>
-      </head>
-      <body>
-        <h1>${title}</h1>
-        <p>${texto1}</p>
-        <p>${texto2}</p>
-      <h2>Imagens:</h2>
-      <ul>
-        ${imagens.map(imagem => `<li><img src="${imagem.src}" alt="${imagem.alt}"></li>`).join('')}
-      </ul>
-    </body>
-    </html>
-  `);
-  newWindow.document.close();
-}
-
 
   // Função para carregar lote de imagens
   function loadImagesBatch() {
